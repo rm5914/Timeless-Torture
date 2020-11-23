161de6d0231c9a66eaa6cd4a7004917e66d1075a
Saumil Savargaonkar <ss4405@ad.rit.edu>
2020-11-23 16:14:25 -0500

    Changes to camra

--- Timeless_Torture/Camera.cs
+++ Timeless_Torture/Camera.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +12,8 @@ namespace Timeless_Torture
     class Camera
     { 
         public Matrix transform;
-        public Rectangle position;
 
         private float zoom;
-        private float rotation;
         private Vector2 centre;
         private Viewport viewport;
 
@@ -32,44 +30,24 @@ namespace Timeless_Torture
             }
         }
 
-        public float Rotation
-        {
-            get { return rotation; }
-            set { rotation = value; }
-        }
-
-        public int X
-        {
-            get { return position.X; }
-            set
-            { 
-                position.X = value; 
-            }
-        }
-
-        public int Y
-        {
-            get { return position.Y; }
-            set
-            {
-                position.Y = value;
-            }
-        }
-
         public Camera(Viewport viewport)
         {
             this.viewport = viewport;
             zoom = 1.0f;
-            rotation = 0.0f;
         }
 
         public void Move(Rectangle center)
         {
-            position = center;
-            transform = Matrix.CreateTranslation(new Vector3(-center.X, -center.Y, 0)) *
-                                                Matrix.CreateRotationZ(Rotation) *
-                                                Matrix.CreateScale(new Vector3(Zoom, Zoom, 1)) *
-                                                Matrix.CreateTranslation(new Vector3(viewport.Width * 0.5f, viewport.Height * 0.5f, 0));
+            var position = Matrix.CreateTranslation(
+                -center.X - (center.Width / 2),
+                -center.Y - (center.Height / 2),
+                0);
+            var offset = Matrix.CreateTranslation(
+                Game1.ScreenWidth / 2,
+                Game1.ScreenHeight / 2,
+                0);
+                                                
+            transform = position * center;
         }
     }
 }
